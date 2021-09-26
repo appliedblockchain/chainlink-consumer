e7f907bed83615621f24ac35d67a3304bcfa3567
Luca <perret.luca@gmail.com>
2021-09-26 20:57:14 +0200

    feat: add Polygon and Mumbai networks (#2)

--- dapp/src/components/CheckTx.tsx
+++ dapp/src/components/CheckTx.tsx
@@ -8,6 +8,8 @@ const explorers: { [chainID: string]: string } = {
   "1": "https://etherscan.io",
   "4": "https://rinkeby.etherscan.io",
   "5": "https://goerli.etherscan.io",
+  "137": "https://polygonscan.com",
+  "80001": "https://mumbai.polygonscan.com",
 };
 
 interface Props {
--- devops/helper-hardhat-config.ts
+++ devops/helper-hardhat-config.ts
@@ -66,6 +66,26 @@ export const networkConfig: { [key: string]: NetworkConfig } = {
     linkToken: "0x326c977e6efc84e512bb9c30f76e30c160ed06fb",
     fundAmount: "0",
   },
+  "137": {
+    name: "polygon",
+    linkToken: "0xb0897686c545045aFc77CF20eC7A532E3120E0F1",
+    ethUsdPriceFeed: "0xF9680D99D6C9589e2a93a78A04A279e509205945",
+    keyHash:
+      "0xf86195cf7690c55907b2b611ebb7343a6f649bff128701cc542f0569e2c549da",
+    vrfCoordinator: "0x3d2341ADb2D31f1c5530cDC622016af293177AE0",
+    fee: "100000000000000",
+    fundAmount: "1000000000000000000",
+  },
+  "80001": {
+    name: "mumbai",
+    linkToken: "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
+    ethUsdPriceFeed: "0x0715A7794a1dc8e42615F059dD6e406A6594651A",
+    keyHash:
+      "0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4",
+    vrfCoordinator: "0x8C7382F9D8f56b33781fE506E897a4F1e2d17255",
+    fee: "100000000000000",
+    fundAmount: "1000000000000000000",
+  },
 };
 
 export const developmentChains = ["hardhat", "localhost"];
